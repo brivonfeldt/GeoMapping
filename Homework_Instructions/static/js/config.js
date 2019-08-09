a57ecbfd7bae4e62b00c79c6d4c47e7f16f9efa1
brivonfeldt <47545914+brivonfeldt@users.noreply.github.com>
2019-08-08 22:04:00 -0600

    uploading files

---
+++ Homework_Instructions/static/js/config.js
@@ -0,0 +1,1 @@
+var API_KEY = "pk.eyJ1IjoiYnJpdjEyMjQiLCJhIjoiY2p4MTMyZDZzMDVrdDRibzd2MDc4Zmh0MyJ9.tJEZ5fVj-Uc8utSLMv9dYA";
---
+++ Homework_Instructions/static/js/logic.js
@@ -0,0 +1,114 @@
+// Store our API endpoint inside queryUrl
+var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
+  "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+// Grab the data with d3
+d3.json(url, function(response) {
+
+    // Create a new marker cluster group
+    var markers = L.markerClusterGroup();
+  
+    // Loop through data
+    for (var i = 0; i < response.length; i++) {
+  
+      // Set the data location property to a variable
+      var location = response[i].location;
+  
+      // Check for location property
+      if (location) {
+  
+        // Add a new marker to the cluster group and bind a pop-up
+        markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+          .bindPopup(response[i].descriptor))
+      
+        }
+
+      function getColor(d) {
+        return d < 1 ? 'rgb(255,255,255)' :
+              d < 2  ? 'rgb(255,225,225)' :
+              d < 3  ? 'rgb(255,195,195)' :
+              d < 4  ? 'rgb(255,165,165)' :
+              d < 5  ? 'rgb(255,135,135)' :
+              d < 6  ? 'rgb(255,105,105)' :
+              d < 7  ? 'rgb(255,75,75)' :
+              d < 8  ? 'rgb(255,45,45)' :
+              d < 9  ? 'rgb(255,15,15)' :
+                          'rgb(255,0,0)';
+      }
+
+    }
+  
+    // Add our marker cluster layer to the map
+    myMap.addLayer(markers, getColor);
+  
+  });
+
+function createFeatures(earthquakeData) {
+
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  function onEachFeature(feature, layer) {
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
+  }
+
+  // Create a GeoJSON layer containing the features array on the earthquakeData object
+  // Run the onEachFeature function once for each piece of data in the array
+  var earthquakes = L.geoJSON(earthquakeData, {
+    onEachFeature: onEachFeature
+  });
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Define streetmap and darkmap layers
+  var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  });
+
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Street Map": streetmap,
+    "Dark Map": darkmap
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  var myMap = L.map("map", {
+    center: [
+      37.09, -95.71
+    ],
+    zoom: 5,
+    layers: [streetmap, earthquakes]
+  });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+}
